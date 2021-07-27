684cef7c1e215caff001672cfdde44f821c663b8
Christon Ragavan <christonragavan@gmail.com>
2021-07-27 19:53:47 +0200

    dev_commit

--- scripts/music_xml_parser/parse.py
+++ scripts/music_xml_parser/parse.py
@@ -7,12 +7,17 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(filename)s:%(lineno)d:%(message)s:%(module)s')
-file_handler = logging.FileHandler('logs.log')
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(filename)s:%(lineno)d:%(message)s')
+file_handler = logging.FileHandler('logs.log', 'w')
 file_handler.setFormatter(formatter)
+stream_formatter = logging.Formatter('%(levelname)s:%(name)s:%(lineno)d:%(message)s')
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(stream_formatter)
+
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
+logger.info("Music xml Parser - score to pandas")
 
-logger.debug("Testing the logging function")
-logger.info("Testing the logging function")
-logger.debug(np.random.rand(20))
+logger.debug(np.random.rand(2))
