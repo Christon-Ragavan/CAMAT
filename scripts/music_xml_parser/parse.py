811ade10ef2c7e91e363d356ce6e28c21b2c559c
Christon Ragavan <christonragavan@gmail.com>
2021-07-27 19:38:44 +0200

    dev_commit

--- scripts/music_xml_parser/parse.py
+++ scripts/music_xml_parser/parse.py
@@ -5,4 +5,14 @@ import traceback
 import logging
 import numpy as np
 
-print(np.random.rand(20))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(filename)s:%(lineno)d:%(message)s:%(module)s')
+file_handler = logging.FileHandler('logs.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+logger.debug("Testing the logging function")
+logger.info("Testing the logging function")
+logger.debug(np.random.rand(20))
