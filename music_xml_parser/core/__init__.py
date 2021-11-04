ea0c8e01747e3e00b5b2818c7e50eaa017ced081
Christon Ragavan <christonragavan@gmail.com>
2021-11-04 09:13:45 +0100

    CAMAT 1.0.0

--- music_xml_parser/core/__init__.py
+++ music_xml_parser/core/__init__.py
@@ -23,4 +23,4 @@ from .analyse import ambitus,\
 from .web_scrapper import get_file_from_server, \
     get_files_from_server
 
-from .corpus import analyse_pitch_class
+from .corpus import analyse_pitch_class, analyse_interval, analyse_basic_statistics
